b79320b695d2e84b5711b19615b336d7be640f15
gediminask <tarkim.gediminas@gmail.com>
2018-03-05 20:09:43 +0200

    User authentication installed.

--- app/Estimate.php
+++ app/Estimate.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Project extends Model
+class Estimate extends Model
 {
     
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,11 +65,21 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+
+    <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        {{menu('app')}}
+
+                        <a class="navbar-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
